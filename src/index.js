2889f7da3f353218adf6e2e84e64cebdfdcd2ec9
sbaumgertner <s.v.baumgertner@gmail.com>
2023-05-01 01:06:14 +0400

    feat: implement page generation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('Virtual keyboard');
+import keys from './js/keyboard.json' assert {type: 'json'};
+import { Key } from './js/Key.js';
+
+function generateRow (keysArray){
+    let row = document.createElement('div');
+    row.className = 'keyboard__row';
+    for (let keyData of keysArray){
+        let key =  new Key(keyData);
+        row.appendChild(key.generateHTML());
+    }
+    return row;
+}
+
+function generateKeyboard(data){
+    let keyboard = document.createElement('div');
+    keyboard.className = 'keyboard';
+    for (let dataRow of data){
+        keyboard.appendChild(generateRow(dataRow));
+    }
+    return keyboard;
+}
+
+function generateDocument(){
+    let wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+    wrapper.innerHTML = '<h1 class="h1">Virtual keyboard</h1>' +
+    '<textarea class="keyboard-input"></textarea>';
+    wrapper.appendChild(generateKeyboard(keys));
+
+    let os = document.createElement('p');
+    os.className = 'os-info';
+    os.innerHTML = 'Created in the Windows OS';
+    wrapper.appendChild(os);
+
+    let lang = document.createElement('p');
+    lang.className = 'lang-info';
+    lang.innerHTML = 'Switch language: left ctrl + alt';
+    wrapper.appendChild(lang);
+
+    document.body.appendChild(wrapper);
+}
+
+generateDocument();
